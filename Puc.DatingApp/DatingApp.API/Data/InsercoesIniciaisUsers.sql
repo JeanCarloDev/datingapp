b69e6e4bd14be21c715afe1dcbe5e8fe89a6eabb
JeanCarloDev <41553588+JeanCarloDev@users.noreply.github.com>
2022-06-02 11:09:17 -0300

    Add API inicial de usuários

---
+++ Puc.DatingApp/DatingApp.API/Data/InsercoesIniciaisUsers.sql
@@ -0,0 +1,9 @@
+-- SQLite
+INSERT INTO Users (Id, Username)
+VALUES (1, "Aluno");
+
+INSERT INTO Users (Id, Username)
+VALUES (2, "PucMinas");
+
+INSERT INTO Users (Id, Username)
+VALUES (3, "ProjetoIntegrado");
